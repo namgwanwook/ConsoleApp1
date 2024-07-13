d5201c4338ec9bffc3a7555e11fa0397abca4980
namgwanwook <2721pooh@naver.com>
2024-07-13 22:13:23 +0900

    C# 최초의 변수 최초의 커밋 최초의 콘솔

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+bool boolVal = true;
+int intVal = 10;
+float floatVal = 3.4f;
+char charVal = 'a';
+
+Console.WriteLine("boolVal : " + boolVal);
+Console.WriteLine("intVal : "+ intVal);
+Console.WriteLine("floatVal : "+floatVal);
+Console.WriteLine("charVal : "+charVal);
+
+
+
